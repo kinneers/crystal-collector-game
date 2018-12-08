3261a8c7d1a01394168a94a49aa9ae7553af485a
Sarah Kinneer <kinneers@gmail.com>
2018-12-08 15:08:00 -0500

    Pseudocoded

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,32 @@
 <script src="https://code.jquery.com/jquery.js"></script>
 
 $(document).ready(function() {
-    
+
+    var wins = 0;
+    var losses = 0;
+    var 
+    /*Pseudocode:
+
+    Variables Needed:
+    wins
+    losses
+    Random Number
+    Each Crystal's Random Number
+    All the clicks added together so far (initialize at 0)
+
+    If the added together so far is less than random number:
+    When a crystal is first clicked unhide value:
+    Each time a crystal is clicked, add that crystal's number to the clicks added array
+    get sum of clicks added array
+
+    If the clicks added together === random number:
+    win!- add to wins
+    reset
+
+    If the clicks added together is greater than random number:
+    lose- add to losses
+    reset
+
+    */
 
 })
