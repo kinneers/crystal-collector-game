59a968fb928475d1606c1c90f521e75f3db9370f
Sarah Kinneer <kinneers@gmail.com>
2018-12-08 18:20:54 -0500

    All game functions are working EXCEPT showing crystal value upon click- css needs work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,32 +1,60 @@
-<script src="https://code.jquery.com/jquery.js"></script>
-
 $(document).ready(function() {
 
     var wins = 0;
     var losses = 0;
-    var 
-    /*Pseudocode:
+    var targetNumber = Math.floor(Math.random() * 120) + 1;
+    var counter = 0;
+    var crystalValueOptions = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1];
+    var crystalURLs = ["assets/images/my-icons-collection/png/002-crystal-1.png", 
+                    "assets/images/my-icons-collection/png/001-crystal.png", 
+                    "assets/images/my-icons-collection/png/003-mineral.png", 
+                    "assets/images/my-icons-collection/png/004-crystal-2.png"];
+    var crystalsAlt = ["Purple Crystal", "Teal Crystal", "Pink Crystal", "Blue Crystal"];
 
-    Variables Needed:
-    wins
-    losses
-    Random Number
-    Each Crystal's Random Number
-    All the clicks added together so far (initialize at 0)
+    console.log(crystalValueOptions);
 
-    If the added together so far is less than random number:
-    When a crystal is first clicked unhide value:
-    Each time a crystal is clicked, add that crystal's number to the clicks added array
-    get sum of clicks added array
+    $("#targetNumberText").text("Your target number is: " + targetNumber);
 
-    If the clicks added together === random number:
-    win!- add to wins
-    reset
+    for (var i = 0; i < crystalValueOptions.length; i++) {
+        var imageCrystal = $("<img>)");
+        imageCrystal.addClass("crystal-image");
+        imageCrystal.attr("src", crystalURLs[i]);
+        imageCrystal.attr("data-crystalValue", crystalValueOptions[i]);
+        imageCrystal.attr("alt", crystalsAlt[i]);
+        $("#crystalButtons").append(imageCrystal);
+    }
 
-    If the clicks added together is greater than random number:
-    lose- add to losses
-    reset
+    function reset() {
+        targetNumber = Math.floor(Math.random() * 120) + 1;
+        counter = 0;
+        crystalValueOptions = [Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1, Math.floor(Math.random() * 12) + 1];
+        crystalURLs = ["assets/images/my-icons-collection/png/002-crystal-1.png", 
+                        "assets/images/my-icons-collection/png/001-crystal.png", 
+                        "assets/images/my-icons-collection/png/003-mineral.png", 
+                        "assets/images/my-icons-collection/png/004-crystal-2.png"];
+        crystalsAlt = ["Purple Crystal", "Teal Crystal", "Pink Crystal", "Blue Crystal"];
+        $("#targetNumberText").text("Your target number is: " + targetNumber);
+        $("#guessTotal").text("Current Score: " + counter);
+    }
+    
+    $(".crystal-image").on("click", function() {
+        var crystalValue = ($(this).attr("data-crystalValue"));
+        crystalValue = parseInt(crystalValue);
+        counter += crystalValue;
+        $("#guessTotal").text("Current Score: " + counter);
+        $("crystal-image").text(crystalValue);
 
-    */
+        if (counter === targetNumber) {
+            wins++;
+            $('#winsText').text("Wins: " + wins);
+            reset();
+        }
 
+        else if (counter >= targetNumber) {
+            alert("You lose!!");
+            losses++;
+            $('#lossesText').text("Losses: " + losses);
+            reset();           
+        } 
+    })
 })
